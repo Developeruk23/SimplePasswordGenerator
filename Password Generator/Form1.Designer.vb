5ee83d3f36c80fd8baa9a388bea741e7f831ba76
Marek Dev <marek@unturnedservermanager.com>
2017-03-14 19:21:36 +0000

    Initial Upload

---
+++ Password Generator/Form1.Designer.vb
@@ -0,0 +1,227 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.BunifuElipse1 = New ns1.BunifuElipse(Me.components)
+        Me.BunifuGradientPanel1 = New ns1.BunifuGradientPanel()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.lbPasswordlist = New System.Windows.Forms.ListBox()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.NumericUpDown2 = New System.Windows.Forms.NumericUpDown()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.NumericUpDown1 = New System.Windows.Forms.NumericUpDown()
+        Me.btnGenerate = New ns1.BunifuThinButton2()
+        Me.btnClear = New ns1.BunifuThinButton2()
+        Me.BunifuGradientPanel1.SuspendLayout()
+        Me.GroupBox1.SuspendLayout()
+        CType(Me.NumericUpDown2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.NumericUpDown1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'BunifuElipse1
+        '
+        Me.BunifuElipse1.ElipseRadius = 5
+        Me.BunifuElipse1.TargetControl = Me
+        '
+        'BunifuGradientPanel1
+        '
+        Me.BunifuGradientPanel1.BackgroundImage = CType(resources.GetObject("BunifuGradientPanel1.BackgroundImage"), System.Drawing.Image)
+        Me.BunifuGradientPanel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.BunifuGradientPanel1.Controls.Add(Me.Label1)
+        Me.BunifuGradientPanel1.Dock = System.Windows.Forms.DockStyle.Top
+        Me.BunifuGradientPanel1.GradientBottomLeft = System.Drawing.Color.Red
+        Me.BunifuGradientPanel1.GradientBottomRight = System.Drawing.Color.Blue
+        Me.BunifuGradientPanel1.GradientTopLeft = System.Drawing.Color.White
+        Me.BunifuGradientPanel1.GradientTopRight = System.Drawing.Color.White
+        Me.BunifuGradientPanel1.Location = New System.Drawing.Point(0, 0)
+        Me.BunifuGradientPanel1.Name = "BunifuGradientPanel1"
+        Me.BunifuGradientPanel1.Quality = 10
+        Me.BunifuGradientPanel1.Size = New System.Drawing.Size(534, 51)
+        Me.BunifuGradientPanel1.TabIndex = 0
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.BackColor = System.Drawing.Color.Transparent
+        Me.Label1.Font = New System.Drawing.Font("Arial Rounded MT Bold", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(47, 13)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(218, 23)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Password Generator "
+        '
+        'lbPasswordlist
+        '
+        Me.lbPasswordlist.FormattingEnabled = True
+        Me.lbPasswordlist.ItemHeight = 16
+        Me.lbPasswordlist.Location = New System.Drawing.Point(12, 66)
+        Me.lbPasswordlist.Name = "lbPasswordlist"
+        Me.lbPasswordlist.Size = New System.Drawing.Size(510, 148)
+        Me.lbPasswordlist.TabIndex = 1
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.NumericUpDown1)
+        Me.GroupBox1.Controls.Add(Me.Label3)
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Controls.Add(Me.NumericUpDown2)
+        Me.GroupBox1.Location = New System.Drawing.Point(12, 230)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(510, 65)
+        Me.GroupBox1.TabIndex = 2
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Advanced Options"
+        '
+        'NumericUpDown2
+        '
+        Me.NumericUpDown2.BackColor = System.Drawing.SystemColors.AppWorkspace
+        Me.NumericUpDown2.Location = New System.Drawing.Point(160, 32)
+        Me.NumericUpDown2.Margin = New System.Windows.Forms.Padding(4)
+        Me.NumericUpDown2.Maximum = New Decimal(New Integer() {30, 0, 0, 0})
+        Me.NumericUpDown2.Minimum = New Decimal(New Integer() {2, 0, 0, 0})
+        Me.NumericUpDown2.Name = "NumericUpDown2"
+        Me.NumericUpDown2.Size = New System.Drawing.Size(83, 22)
+        Me.NumericUpDown2.TabIndex = 8
+        Me.NumericUpDown2.Value = New Decimal(New Integer() {2, 0, 0, 0})
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Segoe UI", 9.0!, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, CType(186, Byte))
+        Me.Label2.Location = New System.Drawing.Point(18, 31)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(120, 20)
+        Me.Label2.TabIndex = 9
+        Me.Label2.Text = "Password Lenght:"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Segoe UI", 9.0!, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, CType(186, Byte))
+        Me.Label3.Location = New System.Drawing.Point(273, 31)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(68, 20)
+        Me.Label3.TabIndex = 10
+        Me.Label3.Text = "Quantity:"
+        '
+        'NumericUpDown1
+        '
+        Me.NumericUpDown1.BackColor = System.Drawing.SystemColors.AppWorkspace
+        Me.NumericUpDown1.Location = New System.Drawing.Point(357, 32)
+        Me.NumericUpDown1.Margin = New System.Windows.Forms.Padding(4)
+        Me.NumericUpDown1.Maximum = New Decimal(New Integer() {10000000, 0, 0, 0})
+        Me.NumericUpDown1.Minimum = New Decimal(New Integer() {1, 0, 0, 0})
+        Me.NumericUpDown1.Name = "NumericUpDown1"
+        Me.NumericUpDown1.Size = New System.Drawing.Size(95, 22)
+        Me.NumericUpDown1.TabIndex = 11
+        Me.NumericUpDown1.Value = New Decimal(New Integer() {1, 0, 0, 0})
+        '
+        'btnGenerate
+        '
+        Me.btnGenerate.ActiveBorderThickness = 1
+        Me.btnGenerate.ActiveCornerRadius = 20
+        Me.btnGenerate.ActiveFillColor = System.Drawing.Color.SeaGreen
+        Me.btnGenerate.ActiveForecolor = System.Drawing.Color.White
+        Me.btnGenerate.ActiveLineColor = System.Drawing.Color.SeaGreen
+        Me.btnGenerate.BackColor = System.Drawing.SystemColors.AppWorkspace
+        Me.btnGenerate.BackgroundImage = CType(resources.GetObject("btnGenerate.BackgroundImage"), System.Drawing.Image)
+        Me.btnGenerate.ButtonText = "Generate"
+        Me.btnGenerate.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.btnGenerate.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnGenerate.ForeColor = System.Drawing.Color.SeaGreen
+        Me.btnGenerate.IdleBorderThickness = 1
+        Me.btnGenerate.IdleCornerRadius = 20
+        Me.btnGenerate.IdleFillColor = System.Drawing.Color.White
+        Me.btnGenerate.IdleForecolor = System.Drawing.Color.SeaGreen
+        Me.btnGenerate.IdleLineColor = System.Drawing.Color.SeaGreen
+        Me.btnGenerate.Location = New System.Drawing.Point(12, 310)
+        Me.btnGenerate.Margin = New System.Windows.Forms.Padding(5)
+        Me.btnGenerate.Name = "btnGenerate"
+        Me.btnGenerate.Size = New System.Drawing.Size(220, 36)
+        Me.btnGenerate.TabIndex = 3
+        Me.btnGenerate.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'btnClear
+        '
+        Me.btnClear.ActiveBorderThickness = 1
+        Me.btnClear.ActiveCornerRadius = 20
+        Me.btnClear.ActiveFillColor = System.Drawing.Color.SeaGreen
+        Me.btnClear.ActiveForecolor = System.Drawing.Color.White
+        Me.btnClear.ActiveLineColor = System.Drawing.Color.SeaGreen
+        Me.btnClear.BackColor = System.Drawing.SystemColors.AppWorkspace
+        Me.btnClear.BackgroundImage = CType(resources.GetObject("btnClear.BackgroundImage"), System.Drawing.Image)
+        Me.btnClear.ButtonText = "Clear"
+        Me.btnClear.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.btnClear.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnClear.ForeColor = System.Drawing.Color.SeaGreen
+        Me.btnClear.IdleBorderThickness = 1
+        Me.btnClear.IdleCornerRadius = 20
+        Me.btnClear.IdleFillColor = System.Drawing.Color.White
+        Me.btnClear.IdleForecolor = System.Drawing.Color.SeaGreen
+        Me.btnClear.IdleLineColor = System.Drawing.Color.SeaGreen
+        Me.btnClear.Location = New System.Drawing.Point(302, 310)
+        Me.btnClear.Margin = New System.Windows.Forms.Padding(5)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(220, 36)
+        Me.btnClear.TabIndex = 4
+        Me.btnClear.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.AppWorkspace
+        Me.ClientSize = New System.Drawing.Size(534, 360)
+        Me.Controls.Add(Me.btnClear)
+        Me.Controls.Add(Me.btnGenerate)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.lbPasswordlist)
+        Me.Controls.Add(Me.BunifuGradientPanel1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.BunifuGradientPanel1.ResumeLayout(False)
+        Me.BunifuGradientPanel1.PerformLayout()
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        CType(Me.NumericUpDown2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.NumericUpDown1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents BunifuElipse1 As ns1.BunifuElipse
+    Friend WithEvents btnClear As ns1.BunifuThinButton2
+    Friend WithEvents btnGenerate As ns1.BunifuThinButton2
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents NumericUpDown1 As NumericUpDown
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents NumericUpDown2 As NumericUpDown
+    Friend WithEvents lbPasswordlist As ListBox
+    Friend WithEvents BunifuGradientPanel1 As ns1.BunifuGradientPanel
+    Friend WithEvents Label1 As Label
+End Class
---
+++ Password Generator/Form1.vb
@@ -0,0 +1,54 @@
+﻿Public Class Form1
+    Dim pNum As New Random(100)
+    Dim pLowerCase As New Random(500)
+    Dim pUpperCase As New Random(50)
+    Dim password As String
+    Dim RandomSelect As New Random(50)
+
+    Public Function getPassword(ByVal passLength As Integer, Optional ByVal Reset As Boolean = False) As String
+        Dim i As Integer
+        Dim ctr(2) As Integer
+        Dim charSelect(2) As String
+        Dim iSel As Integer
+        If Reset = True Then
+            password = ""
+        End If
+        For i = 1 To passLength
+            ctr(0) = pNum.Next(48, 57)
+            ctr(1) = pLowerCase.Next(65, 90)
+            ctr(2) = pUpperCase.Next(97, 122)
+            charSelect(0) = System.Convert.ToChar(ctr(0)).ToString
+            charSelect(1) = System.Convert.ToChar(ctr(1)).ToString
+            charSelect(2) = System.Convert.ToChar(ctr(2)).ToString
+            iSel = RandomSelect.Next(0, 3)
+            password &= charSelect(iSel)
+            If Reset = True Then
+                password.Replace(password, charSelect(iSel))
+            End If
+        Next
+        Return password
+
+    End Function
+
+    Private Sub BunifuRating1_onValueChanged(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub btnGenerate_Click(sender As Object, e As EventArgs) Handles btnGenerate.Click
+        Dim j As Integer
+        For j = 1 To NumericUpDown1.Value
+            lbPasswordlist.Items.Add(getPassword(NumericUpDown2.Value, True))
+        Next
+        Label1.Text = "Sucessfuly Generated : " & lbPasswordlist.Items.Count & " Passwords!"
+        lbPasswordlist.SelectedIndex = lbPasswordlist.Items.Count - 1
+    End Sub
+
+    Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+        lbPasswordlist.Items.Clear()
+    End Sub
+End Class
+
